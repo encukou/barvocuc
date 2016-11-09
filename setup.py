53b68337ea6b536f8c01a7e282b1f4fe0e59dfda
Petr Viktorin <encukou@gmail.com>
2016-11-09 21:03:17 +0100

    Add test for expected files resulting from analysis

--- setup.py
+++ setup.py
@@ -1,13 +1,45 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import sys
 
-import py2exe
 
-
-setup(
-    zipfile = None,
-    console=['gui.py'],
- excludes = ["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
-                "pywin.dialogs", "pywin.dialogs.list",
-                "Tkconstants","Tkinter","tcl"
-                ]
+setup_args = dict(
+    name='barvocuc',
+    version='2.0b1',
+    description='Software for Analysis of Color Images',
+    #long_description="TODO",
+    author='Petr Viktorin',
+    author_email='encukou@gmail.com',
+    license='GPL v3+',
+    url='https://github.com/encukou/barvocuc',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        ],
+    install_requires=[
+        #'PyQT5',       # Not readily installable via pip
+        'PyYAML',
+        'Pillow',
+        'Numpy',
+        'Scipy',
+        'Click',
+        ],
+    zip_safe=False,
 )
+
+if sys.platform == 'win32':
+    # py2exe arguments
+    setup_args.update(dict(
+        zipfile=None,
+        console=['gui.py'],
+        excludes=["pywin", "pywin.debugger", "pywin.debugger.dbgcon",
+                  "pywin.dialogs", "pywin.dialogs.list",
+                  "Tkconstants", "Tkinter", "tcl",
+                  ]
+    ))
+
+
+setup(**setup_args)
--- test_barvocuc/test_batch.py
+++ test_barvocuc/test_batch.py
@@ -20,3 +20,30 @@ def test_gen_csv_cs(lang):
     with open(os.path.join(basedir, 'expected_{}.csv'.format(lang))) as f:
         expected = f.read()
     assert got == expected
+
+
+def assert_dirs_same(adir, bdir):
+    al = sorted(os.listdir(adir))
+    bl = sorted(os.listdir(bdir))
+    assert al == bl
+    for a, b in zip(al, bl):
+        fulla = os.path.join(adir, a)
+        fullb = os.path.join(bdir, b)
+        print('a =', fulla)
+        print('b =', fullb)
+        if os.path.isdir(fulla):
+            assert os.path.isdir(fullb)
+            assert_dirs_same(fulla, fullb)
+        else:
+            assert not os.path.isdir(fullb)
+            with open(fulla, 'rb') as af, open(fullb, 'rb') as bf:
+                assert af.read() == bf.read()
+
+
+def test_gen_dir(tmpdir):
+    settings = Settings()
+
+    with io.StringIO() as f:
+        generate_csv(f, [basedir], settings=settings, outdir=str(tmpdir))
+        got = f.getvalue()
+    assert_dirs_same(str(tmpdir), os.path.join(basedir, 'expected_dir'))
