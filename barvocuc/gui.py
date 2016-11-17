2ccf4330c04c9e1a897a77266d0cf8d87e440f3c
Petr Viktorin <encukou@gmail.com>
2016-11-17 17:28:44 +0100

    Nicer zooming and background for the graphics views

--- barvocuc/gui.py
+++ barvocuc/gui.py
@@ -1,6 +1,7 @@
 import os
 import gc
 import weakref
+import math
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
@@ -9,6 +10,9 @@ from .analysis import ImageAnalyzer
 
 VIEWS = 'source', 'colors', 'sobel', 'opacity'
 
+
+MAX_ZOOM = 32
+MIN_ZOOM = 1/32
 
 _weakdict = weakref.WeakKeyDictionary()
 
@@ -38,34 +42,52 @@ def qpixmap_from_float_array(array):
 
 
 class SynchronizedGraphicsView(QtWidgets.QGraphicsView):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        self._bgbrush1 = QtGui.QBrush(QtGui.QColor(200, 200, 200))
+        self._bgbrush2 = QtGui.QBrush(QtGui.QColor(100, 100, 100),
+                                      QtCore.Qt.Dense4Pattern)
+
     def wheelEvent(self, event):
-        for self in self.friends:
-            # Zoom Factor
-            zoomInFactor = 1.1
-            zoomOutFactor = 1 / zoomInFactor
+        # Get the cursor's scene position -- this should not change
+        old_pos = self.mapToScene(event.pos())
 
-            # Set Anchors
-            self.setTransformationAnchor(QtWidgets.QGraphicsView.NoAnchor)
-            self.setResizeAnchor(QtWidgets.QGraphicsView.NoAnchor)
+        [item] = self.scene().items()
+        zoom = old_zoom = item.scale()
 
-            # Save the scene pos
-            oldPos = self.mapToScene(event.pos())
+        zoom = math.exp(math.log(zoom) + event.angleDelta().y()/1000)
+        if zoom > MAX_ZOOM:
+            zoom = MAX_ZOOM
+        if zoom < MIN_ZOOM:
+            zoom = MIN_ZOOM
+        for friend in self.friends:
+            friend._zoom(zoom)
 
-            # Zoom
-            if event.angleDelta().y() > 0:
-                zoomFactor = zoomInFactor
-            else:
-                zoomFactor = zoomOutFactor
-            if zoomFactor > 8:
-                zoomFactor = 8
-            self.scale(zoomFactor, zoomFactor)
+        scrollbar_h = self.horizontalScrollBar()
+        scrollbar_v = self.verticalScrollBar()
 
-            # Get the new position
-            newPos = self.mapToScene(event.pos())
+        scrollbar_h.setValue(old_pos.x() / old_zoom * zoom - event.x())
+        scrollbar_v.setValue(old_pos.y() / old_zoom * zoom - event.y())
 
-            # Move scene to old position
-            delta = newPos - oldPos
-            self.translate(delta.x(), delta.y())
+        self.backgroundBrush().setTransform(QtGui.QTransform.fromTranslate(
+            zoom,
+            zoom))
+
+    def _zoom(self, zoom):
+        [item] = self.scene().items()
+        item.setScale(zoom)
+        self.setSceneRect(0, 0,
+                          item.boundingRect().width() * zoom,
+                          item.boundingRect().height() * zoom)
+
+    def drawBackground(self, painter, rect):
+        pt = self.mapFromScene(QtCore.QPoint(0, 0))
+        painter.translate(-pt.x(), -pt.y())
+        painter.scale(4, 4)
+        painter.fillRect(0, 0, self.width(), self.height(), self._bgbrush1)
+        painter.fillRect(0, 0, self.width(), self.height(), self._bgbrush2)
 
 
 class BarvocucMainWindow(QtWidgets.QMainWindow):
